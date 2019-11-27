a3a5da01220c979033899559ce88523784c56505
Sergio Vega <svegasise@gmail.com>
2019-11-27 01:06:34 +0100

    Añadida barra deopciones, da errores

---
+++ src/puzzle2dpentominos/Puzzle2DPentominos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puzzle2dpentominos;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Puzzle2DPentominos {
+
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        new GUI().setVisible(true);
+//    }
+    
+}
